5bc55bf3a4a8911bae1df86c1c124ee43e9b39f2
candy00961 <candy00961@gmail.com>
2025-01-21 17:45:07 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -25,6 +25,36 @@ const config = {
     previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
     accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN',
   },
+  siteMap: ({ documents, modelExtensions }) => {
+    // 1. Filter all page models which were defined in modelExtensions
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                case 'Blog':
+                    return 'otherBlog';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
 };
 
 export default config;
