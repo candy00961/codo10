fc202f6e7cfaf6c3adc1180a40bdc6de5f00099d
candy00961 <candy00961@gmail.com>
2025-01-21 13:25:49 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -6,10 +6,10 @@ const config = {
   nodeVersion: '18',
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
       environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     }),
   ],
   modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
