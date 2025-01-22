12afa2635c01f5e68423c9365c4e23287e7b0ce0
candy00961 <candy00961@gmail.com>
2025-01-22 13:59:54 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,21 +1,21 @@
-import { defineStackbitConfig } from '@stackbit/types';
-
-export default defineStackbitConfig({
-  stackbitVersion: '~2.0.0',
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
+const config = {
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '20.18.1',
   contentSources: [
-    {
-      name: 'contentful',
-      type: 'contentful',
+    new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: 'master',
-      accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN
-    }
-  ],
-  contentModelMap: {
+      environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ], contentModelMap: {
     invoice: { type: 'data' }
   },
-  models: {
+  
+   models: {
     invoice: {
       type: 'data',
       label: 'Invoice',
@@ -55,4 +55,48 @@ export default defineStackbitConfig({
       ]
     }
   }
-});
+  modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
+  // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
+  import: {
+    type: 'contentful',
+    contentFile: 'contentful/export.json',
+    uploadAssets: true,
+    assetsDirectory: 'contentful',
+    spaceIdEnvVar: 'CONTENTFUL_SPACE_ID',
+    deliveryTokenEnvVar: 'CONTENTFUL_DELIVERY_TOKEN',
+    previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
+    accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN',
+  },
+  siteMap: ({ documents, modelExtensions }) => {
+    // 1. Filter all page models which were defined in modelExtensions
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                case 'Blog':
+                    return 'otherBlog';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
+};
+
+export default config;
