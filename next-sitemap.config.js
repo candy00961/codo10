518e843baecab246f1e79e405faf977ab61b07f5
candy00961 <candy00961@gmail.com>
2025-01-23 11:35:04 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://codo10.netlify.com',
+  siteUrl: process.env.SITE_URL || 'https://codo10.netlify.app',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
