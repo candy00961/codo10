ee190a5618784dbdade81f30a9ff5f39392fcd6f
candy00961 <candy00961@gmail.com>
2025-01-27 00:22:11 +0400

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://yourwebsite.com', // Replace with your website URL
+  siteUrl: 'https://codo10.netlify.app', // Replace with your website URL
   generateRobotsTxt: true, // Generate robots.txt file
   sitemapSize: 7000, // Split large sitemaps into multiple files
   exclude: ['/server-sitemap.xml'], // Exclude specific routes
