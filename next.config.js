ff4d77d09c2d8b1df2cc97775fe5ad8eba0ffd0d
candy00961 <candy00961@gmail.com>
2025-02-21 12:58:00 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
   images: {
@@ -17,38 +16,10 @@ const nextConfig = {
     CONTENTFUL_MANAGEMENT_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT || 'master',
   },
-  webpack: (config, { isServer }) => {
-    // Enable source maps for debugging
+  webpack: (config) => {
     config.devtool = 'source-map';
-
-    // Add source-map-loader to process source maps
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader'],
-        exclude: /node_modules/,
-      });
-    }
-
     return config;
   },
 };
 
-const sentryWebpackPluginOptions = {
-  silent: false, // Display logs for debugging
-  org: process.env.SENTRY_ORG || 'cloudperry', // Your Sentry organization slug
-  project: process.env.SENTRY_PROJECT || 'codo10', // Your Sentry project slug
-  release: process.env.COMMIT_REF || 'development', // Release version for Sentry
-  include: '.next', // Include the .next directory for source map uploads
-  ignore: ['node_modules'], // Ignore node_modules
-  urlPrefix: '~/_next', // Set the URL prefix to match Next.js file paths
-  dryRun: process.env.NODE_ENV !== 'production', // Only upload source maps in production
-  setCommits: {
-    auto: true, // Automatically detect commits
-  },
-  debug: false, // Set to false for production to avoid cluttering logs
-  deleteSourcemapsAfterUpload: true, // Prevent serving source maps to users
-};
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig;
