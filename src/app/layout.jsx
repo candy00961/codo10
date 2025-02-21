9ce65ebcd26a16c550d2a33e3b409c0c4bd46f27
candy00961 <candy00961@gmail.com>
2025-02-21 13:00:26 +0400

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,17 @@
-import '../../styles/globals.css';
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview';
 
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <ContentfulLivePreviewProvider
+          space={process.env.CONTENTFUL_SPACE_ID}
+          accessToken={process.env.CONTENTFUL_PREVIEW_TOKEN}
+          enableLiveUpdates={true}
+          locale="en-US"
+        >
+          {children}
+        </ContentfulLivePreviewProvider>
       </body>
     </html>
   );
