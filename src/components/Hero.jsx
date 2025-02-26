1710e15c7765fc0f4e018fb3f787548e5465791d
candy00961 <candy00961@gmail.com>
2025-02-26 09:09:47 +0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import Markdown from 'markdown-to-jsx';
 import Image from 'next/image';
 import { Button } from './Button.jsx';
 
@@ -6,28 +7,28 @@ const themeClassMap = {
   imgRight: 'md:flex-row',
 };
 
-export const Hero = (props) => {
+export const Hero = ({ id, heading, body, button, image, theme }) => {
   return (
-    <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24" data-sb-object-id={props.id}>
-      <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
+    <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24" data-sb-object-id={id}>
+      <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[theme] || themeClassMap['imgRight']}`}>
         <div className="w-full max-w-xl mx-auto flex-1">
           <h1 className="mb-6 text-4xl font-bold sm:text-5xl" data-sb-field-path="heading">
-            {props.heading}
+            {heading || 'Default Heading'}
           </h1>
-          {props.body && (
+          {body && (
             <Markdown options={{ forceBlock: true }} className="mb-6 text-lg" data-sb-field-path="body">
-              {props.body}
+              {body}
             </Markdown>
           )}
-          {props.button && <Button {...props.button} />}
+          {button && <Button {...button} />}
         </div>
         <div className="w-full aspect-[4/3] flex-1 relative overflow-hidden rounded-md">
-          {props.image && (
+          {image && (
             <Image
-              src={props.image.src}
-              alt={props.image.alt}
+              src={image.src || '/placeholder.jpg'}
+              alt={image.alt || 'Hero Image'}
               fill
-              className='object-cover'
+              className="object-cover"
               sizes="(max-width: 767px) 100vw, (max-width: 1200px) 50vw, 600px"
               data-sb-field-path="image"
             />
