08c872c7fe59a13ee9480836a9af6d1b24dde87f
candy00961 <candy00961@gmail.com>
2025-02-26 13:20:20 +0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -72,6 +72,6 @@ async function fetchPageData(pageSlug, isPreview = false) {
     return response.items[0];
   } catch (error) {
     // console.error(`Error fetching page data for slug "${pageSlug}":`, error);
-    return null ;
+    return null;
   }
 }
