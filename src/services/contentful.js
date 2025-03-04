a8f8bf424aa679d162c2703c5114c588b9094285
candy00961 <candy00961@gmail.com>
2025-03-04 11:41:47 +0400

    Update contentful.js

--- src/services/contentful.js
+++ src/services/contentful.js
@@ -1,15 +1,13 @@
-src/services/contentful.js
+import { createClient } from 'contentful';
+
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
 });
-module.exports = client;
+
+export default client;
+
 export async function fetchEntries() {
-  try {
-    const entries = await client.getEntries();
-    return entries.items;
-      } catch (error) {
-    // console.error('Error fetching entries:', error);
-    throw error;
-      }
+  const entries = await client.getEntries();
+  return entries.items;
 }
