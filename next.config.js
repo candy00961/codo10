b4ba03810de4a450ee884c4ed13a3d0e734a9db5
candy00961 <candy00961@gmail.com>
2025-04-15 13:08:22 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,32 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.ctfassets.net', // Allow images from Contentful
+        hostname: 'images.ctfassets.net',
       },
     ],
   },
-  productionBrowserSourceMaps: true, // Enable source maps for production builds
+  productionBrowserSourceMaps: true,
   env: {
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_DELIVERY_TOKEN,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_PREVIEW_TOKEN: process.env.CONTENTFUL_PREVIEW_TOKEN,
-    CONTENTFUL_MANAGEMENT_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-    CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT || 'master',
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.devtool = 'source-map';
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      });
+    }
     return config;
   },
+  // cacheHandler: require('./cache-handler.js'), // Temporarily disabled due to build error
+  // cacheMaxMemorySize: 0, // Temporarily disabled
 };
 
 module.exports = nextConfig;
