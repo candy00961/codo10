9a22d0b7ed2d2bd154c5283e9e166fa8d1ac7811
candy00961 <candy00961@gmail.com>
2025-04-16 09:43:35 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 const nextConfig = {
   images: {
     remotePatterns: [
