7a20cb450d980b703c5089e8ca96e0ba16ddea40
candy00961 <candy00961@gmail.com>
2025-04-16 11:03:00 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,8 @@ import { getPageFromSlug } from '../utils/content.js'; // Verify path
 const componentMap = {
   hero: Hero,
   stats: Stats,
+  button: Button,
+  homePage: Homepage
   // Add mappings for any other section types you might create
 };
 
