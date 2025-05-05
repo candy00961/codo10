75ee50bb127811f7ea61a1b35b2c2db386ffbe26
candy00961 <candy00961@gmail.com>
2025-05-05 10:42:11 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -38,6 +38,10 @@ export default defineStackbitConfig({
       type: 'page',
       urlPath: '/invoices/{slug}',
     },
+    {
+      name:'homepage',
+      type: 'page',
+    }
     // --- Change type to "data" for component/data models ---
     { name: 'hero', type: 'data' },
     { name: 'stats', type: 'data' },
