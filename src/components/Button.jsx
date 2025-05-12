a287d213b3d877ee8df43314a122affb51e71948
candy00961 <candy00961@gmail.com>
2025-05-12 16:13:55 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,20 @@
 import Link from 'next/link';
 
-export function Button({ href, label }) {
-  if (!href) {
-    if (process.env.NODE_ENV === 'development') {
-      console.warn('Missing href in <Button>');
-    }
-    return null; // Prevent Link error
-  }
+const themeClassMap = {
+  default: 'border-purple-700 bg-purple-700 text-white hover:bg-purple-500 hover:border-purple-500',
+  outline: 'border-purple-700 bg-transparent text-purple-700 hover:text-purple-500 hover:border-purple-500',
+};
 
+export const Button = (props) => {
   return (
-    <Link href={href}>
-      <button>{label}</button>
+    <Link
+      href={props.url}
+      className={`py-3 px-6 inline-block border-2 font-semibold rounded-md transition-all duration-300 ${
+        themeClassMap[props.theme] ?? themeClassMap['default']
+      }`}
+      data-sb-object-id={props.id}
+    >
+      <span data-sb-field-path="label">{props.label}</span>
     </Link>
   );
-}
+};
